ab794404dcdba2fc7cb5f6b48786915fd316fef7
Jiho Lee <baleen37@gmail.com>
2024-03-20 14:26:36 +0900

    chore: fix

--- init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS youtube_posts
-(
-    id          SERIAL PRIMARY KEY,
-    channel_id  VARCHAR(255),
-    title       VARCHAR(255),
-    created_at  TIMESTAMP
-);
--- src/application/AutoRecorderApplication.ts
+++ src/application/AutoRecorderApplication.ts
@@ -3,19 +3,12 @@ import { Service } from 'typedi';
 
 @Service()
 export class AutoRecorderApplication {
-  constructor(
-    public recorderService: RecorderService,
-  ) {}
+  constructor(public recorderService: RecorderService) {}
 
-  async record(input: {
-    summonerName: string,
-    gameId: number,
-  }) {
-    await this.recorderService.record(
-      {
-        summonerName: input.summonerName,
-        gameId: input.gameId,
-      }
-    );
+  async record(input: { summonerName: string; gameId: number }) {
+    await this.recorderService.record({
+      summonerName: input.summonerName,
+      gameId: input.gameId,
+    });
   }
 }
