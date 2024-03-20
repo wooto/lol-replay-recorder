ac67e1ccc1270547cda60c00bef9fd39c1105d26
Jiho Lee <baleen37@gmail.com>
2024-03-20 14:53:18 +0900

    test: add test

---
+++ test/league-client.ts
@@ -0,0 +1,10 @@
+import { LeagueClient } from '../src/usecases/recorder/apis/league-client';
+import { expect } from 'chai';
+
+describe('league-client', () => {
+  it('should be tested', async () => {
+    let leagueClient = new LeagueClient();
+    const path = await leagueClient.getRoflsPath();
+    expect(path).to.not.null;
+  });
+});
