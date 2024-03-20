5e6e4dc54fccdc6c0f5eb1d692799be121da7e48
Jiho Lee <baleen37@gmail.com>
2024-03-20 15:36:49 +0900

    test: add test

--- test/e2e/league-client.ts
+++ test/e2e/league-client.ts
@@ -2,6 +2,11 @@ import { LeagueClient } from '../../src/usecases/recorder/apis/league-client';
 import { expect } from 'chai';
 
 describe('league-client', () => {
+  const getSummoner = async () => {
+    let leagueClient = new LeagueClient();
+    return await leagueClient.getCurrentSummoner();
+  };
+
   it('should getRoflsPath to be not null', async () => {
     let leagueClient = new LeagueClient();
     const path = await leagueClient.getRoflsPath();
@@ -48,5 +53,19 @@ describe('league-client', () => {
     let leagueClient = new LeagueClient();
     const settings = await leagueClient.enableWindowMode();
     expect(settings).to.not.null;
+  });
+
+  it('should getMatchHistoryByPuuid to be not null', async () => {
+    let leagueClient = new LeagueClient();
+    const { puuid } = await getSummoner();
+    const matchHistory = await leagueClient.getMatchHistoryByPuuid(puuid, 0, 10);
+    expect(matchHistory).to.not.null;
+  });
+
+  it('should getSummonersByRiotId to be not null', async () => {
+    let leagueClient = new LeagueClient();
+    const { riotId } = await getSummoner();
+    const summoners = await leagueClient.getSummonersByRiotId(riotId);
+    expect(summoners).to.not.null;
   });
 });
