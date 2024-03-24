fcff0cf20f8595e6914af1ecc7c1e9003eee0f58
Jiho Lee <baleen37@gmail.com>
2024-03-24 19:26:48 +0900

    refactor: repackage

--- test/e2e/LeageuClientExecutable.test.ts
+++ test/e2e/LeageuClientExecutable.test.ts
@@ -1,28 +1,28 @@
-import LeagueClientExecutable from '../../src/apis/LeagueClientExecution';
+import { LeagueClientExecution } from '../../src/apis/LeagueClientExecution';
 import { expect } from 'chai';
 
 describe('LeagueClientExecutable', () => {
   it('should be able to find the LeagueClient executable', async () => {
-    const paths = await LeagueClientExecutable.getInstalledPaths();
+    const paths = await new LeagueClientExecution().getInstalledPaths();
     expect(paths).not.to.be.empty;
   });
 
   it('should be able to find the LeagueClient config file', async () => {
-    const paths = await LeagueClientExecutable.getConfigFilePaths();
+    const paths = await new LeagueClientExecution().getConfigFilePaths();
     expect(paths).not.to.be.empty;
   });
 
   it('should be able to enable the LeagueClient config file, set false', async () => {
-    const paths = await LeagueClientExecutable.getConfigFilePaths();
-    await LeagueClientExecutable.setGameEnabled(paths[0], false);
-    const enabled = await LeagueClientExecutable.isGameEnabled(paths[0]);
+    const paths = await new LeagueClientExecution().getConfigFilePaths();
+    await new LeagueClientExecution().setGameEnabled(paths[0], false);
+    const enabled = await new LeagueClientExecution().isGameEnabled(paths[0]);
     expect(enabled).to.be.false;
   });
 
   it('should be able to enable the LeagueClient config file, set true', async () => {
-    const paths = await LeagueClientExecutable.getConfigFilePaths();
-    await LeagueClientExecutable.setGameEnabled(paths[0], true);
-    const enabled = await LeagueClientExecutable.isGameEnabled(paths[0]);
+    const paths = await new LeagueClientExecution().getConfigFilePaths();
+    await new LeagueClientExecution().setGameEnabled(paths[0], true);
+    const enabled = await new LeagueClientExecution().isGameEnabled(paths[0]);
     expect(enabled).to.be.true;
   });
 
