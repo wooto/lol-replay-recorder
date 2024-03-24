f5405c10a92536e507e59e46ac6f400b64474745
Jiho Lee <baleen37@gmail.com>
2024-03-24 16:06:14 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export { LolRecorderService } from './usecases/recorder/LoLRecorderService';
-export * as LeagueClient from './usecases/recorder/apis/LeagueClientUx';
+export * as RiotGameClient from './usecases/recorder/apis/RiotGameClient';
+export * as LeagueClientUx from './usecases/recorder/apis/LeagueClientUx';
+export * as ReplayClient from './usecases/recorder/apis/ReplayClient';
+export * as LeagueClientExecutable from './usecases/recorder/apis/LeagueClientExecutable';
 
