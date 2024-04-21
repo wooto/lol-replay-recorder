e5d9deeedba38765c1bd9bf77b3ed3896a54a90d
Jiho Lee <baleen37@gmail.com>
2024-04-21 18:14:32 +0900

    test: test

--- example/watch.ts
+++ example/watch.ts
@@ -30,10 +30,15 @@ for (const locale of locale_list) {
   await new RiotGameClient().login(username, password);
   await sleepInSeconds(10);
   for(let i = 0; i < 5; i++) {
-    console.log('Started client');
-    await new LeagueClientUx().startClient({ region, locale: locale });
-    console.log('Waiting for client to be ready');
-    await new LeagueClientUx().getState({ options: { retry: 10 } });
+    try{
+      console.log('Started client');
+      await new LeagueClientUx().startClient({ region, locale: locale });
+      console.log('Waiting for client to be ready');
+      await new LeagueClientUx().getState({ options: { retry: 10 } });
+    } catch (e) {
+      console.log('Failed to start client:', e);
+      await sleepInSeconds(1);
+    }
   }
   console.log('Client is running');
   await sleepInSeconds(5);
