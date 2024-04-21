df0f3ab32c5aa750b09ecc41a94a55427c1414e7
Jiho Lee <baleen37@gmail.com>
2024-04-21 18:33:23 +0900

    test: test

--- example/watch.ts
+++ example/watch.ts
@@ -1,7 +1,6 @@
 import { exit } from 'node:process';
-import { LeagueClientExecution, LeagueClientUx, RiotGameClient } from '../src';
+import { LeagueClientExecution } from '../src';
 import { Locale } from '../src/model/Locale';
-import { sleepInSeconds } from '../src/utils/utils';
 
 
 const username = process.env.RIOT_USERNAME;
@@ -10,35 +9,11 @@ console.log('Hello, World!');
 
 const locale_list = Object.values(Locale);
 for (const locale of locale_list) {
-  console.log('Starting client with locale:', locale)
-  const region = 'na1';
-  await new LeagueClientExecution().stopRiotProcesses();
-  console.log('Stopped Riot processes')
-  await new RiotGameClient().startRiotClient(region as any, locale);
-  await new LeagueClientExecution().stopRiotProcesses();
-  console.log('Stopped Riot processes')
-
-  for(let i = 0; i < 5; i++) {
-    try{
-      await new RiotGameClient().startRiotClient(region as any, locale);
-      await new RiotGameClient().login(username, password);
-      console.log('Started client');
-      await new LeagueClientUx().startClient({ region, locale: locale });
-      console.log('Waiting for client to be ready');
-      const { action } = await new LeagueClientUx().getState({ options: { retry: 10 } });
-      console.log('Client state:', action);
-      if (action !== 'Idle') {
-        throw new Error('Client is not ready', action);
-      }
-      console.log('Client is ready');
-      break;
-    } catch (e) {
-      console.log('Failed to start client:', e);
-      await sleepInSeconds(1);
-    }
-  }
-  console.log('Client is running');
-  await sleepInSeconds(5);
-  console.log('Waiting for 5 seconds');
+  new LeagueClientExecution().startRiotProcessesSafely({
+    region: 'na1' as any,
+    locale,
+    username,
+    password,
+  });
 }
 exit(0);
