633a8d109ad68e441d2a30e55c6e4de0587764de
Jiho Lee <baleen37@gmail.com>
2024-04-21 16:27:20 +0900

    test: test

---
+++ example/watch.ts
@@ -0,0 +1,19 @@
+import { exit } from 'node:process';
+import { LeagueClientExecution, LeagueClientUx, RiotGameClient } from '../src';
+import { Locale } from '../src/model/Locale';
+
+
+const username = process.env.RIOT_USERNAME;
+const password = process.env.RIOT_PASSWORD;
+console.log('Hello, World!');
+
+const locale_list = Object.values(Locale);
+for (const locale of locale_list) {
+  const region = 'na1';
+  await new LeagueClientExecution().stopRiotProcesses();
+
+  await new LeagueClientUx().startClient({ region, locale: locale });
+  await new RiotGameClient().login(username, password);
+  await new RiotGameClient().isRunning();
+}
+exit(0);
