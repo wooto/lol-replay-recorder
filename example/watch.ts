8cdb8162d852e3a533795499abdb9a40702b25d8
Jiho Lee <baleen37@gmail.com>
2024-04-21 17:45:59 +0900

    test: test

--- example/watch.ts
+++ example/watch.ts
@@ -13,15 +13,19 @@ for (const locale of locale_list) {
   console.log('Starting client with locale:', locale)
   const region = 'na1';
   await new LeagueClientExecution().stopRiotProcesses();
+  console.log('Stopped Riot processes')
 
-  await new LeagueClientUx().startClient({ region, locale: locale });
+  await new RiotGameClient().startRiotClient(region as any, locale);
   console.log('Client started');
   await new RiotGameClient().login(username, password);
   console.log('Logged in');
   await new RiotGameClient().getInstalls();
+  console.log('Got installs');
   await new LeagueClientExecution().stopRiotProcesses();
+  console.log('Stopped Riot processes');
 
   await new LeagueClientUx().startClient({ region, locale: locale });
+  console.log('Started client');
   await new LeagueClientUx().getState();
   console.log('Client is running');
   await sleepInSeconds(5);
