7f7916e2f0fe48d47e34a96df01e428f416086a5
jito <baleen37@gmail.com>
2024-10-13 12:06:35 +0900

    chore: fix test

--- test/unit/apis/IniEditor.test.ts
+++ test/unit/apis/IniEditor.test.ts
@@ -2,16 +2,21 @@ import { expect } from "chai";
 import { IniEditor } from "../../../src/apis/IniEditor";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import fs from "fs";
+import { tmpdir } from "os";
 
 
 const __filename = fileURLToPath(import.meta.url);
-
 const __dirname = dirname(__filename);
 
+
 describe('IniEditor', () => {
+    beforeEach(() => {
+        fs.copyFileSync(path.join(__dirname, 'test.ini'), path.join(tmpdir(), 'test.ini'));
+    })
 
     it('load test.ini', async () => {
-        const inieditor = new IniEditor(path.join(__dirname, 'test.ini'));
+        const inieditor = new IniEditor(path.join(tmpdir(), 'test.ini'));
 
         expect(inieditor.data).to.be.not.null;
         expect(inieditor.data.test).to.eql("1")
@@ -20,13 +25,13 @@ describe('IniEditor', () => {
     });
 
     it('update test.ini', async () => {
-        const inieditor = new IniEditor(path.join(__dirname, 'test.ini'));
+        const inieditor = new IniEditor(path.join(tmpdir(), 'test.ini'));
         inieditor.update('test', '2');
         inieditor.update('test.a', '2-a');
         inieditor.update('test.b', '2-b');
         inieditor.save();
 
-        const inieditor2 = new IniEditor(path.join(__dirname, 'test.ini'));
+        const inieditor2 = new IniEditor(path.join(tmpdir(), 'test.ini'));
         expect(inieditor2.data).to.be.not.null;
         expect(inieditor2.data.test).to.eql("2")
         expect(inieditor2.data['test.a']).to.eql("2-a")
